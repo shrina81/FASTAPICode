84fae15d03550a665dcb8d30e2f029bd5d3eeb09
shrina81 <67715643+shrina81@users.noreply.github.com>
2023-03-30 19:04:21 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -44,6 +44,6 @@ async def read_root(request: Request):
     return response
 
 if __name__ == "__main__":
-    uvicorn.run("main:app",host='0.0.0.0', port=8080, reload=True)
+    uvicorn.run("main:app",host='0.0.0.0', port=8080, reload=True,debug=True)
 
 
